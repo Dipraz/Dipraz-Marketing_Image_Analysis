0aa490e792937180c478a6e53f9cd849a6a23de2
Dipraj Howlader <118705873+Dipraz@users.noreply.github.com>
2024-05-21 18:34:05 +0600

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -23,7 +23,7 @@ else:
 
     # Define generation configuration
     generation_config = {
-        "temperature": 0.3,
+        "temperature": 0.4,
         "top_p": 0.95,
         "top_k": 64,
         "max_output_tokens": 8192,
