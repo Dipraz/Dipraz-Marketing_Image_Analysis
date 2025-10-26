64da9b1b539b5c58e34f4ea9060dfa2ed86b614b
Dipraj Howlader <118705873+Dipraz@users.noreply.github.com>
2025-10-26 12:27:07 +0600

    Update video.py

--- video.py
+++ video.py
@@ -56,7 +56,7 @@ def analyze_video(uploaded_video, prompt, temperature, top_p, max_tokens):
             "top_p": top_p,
         }
 
-        model = GenerativeModel("gemini-flash-lite-latest")
+        model = GenerativeModel("gemini-2.5-flash")
 
         start_time = time.time()
         with st.spinner('Analyzing the video... This might take a few moments.'):
@@ -126,6 +126,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
